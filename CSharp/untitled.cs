df1377480486d1af77e818e40cacd4ff489b2cad
Karan Jaswani <karanjaswani123@gmail.com>
2016-09-22 00:39:59 -0500

    Initial Commit.
    This is my first commit.

---
+++ CSharp/untitled.cs
@@ -0,0 +1,5 @@
+public class Solution {
+    public string Convert(string s, int numRows) {
+      string a[][] = new string[][];
+    }
+}
---
+++ Python/SampleProject/src/Solver.py
@@ -0,0 +1,15 @@
+import math
+
+_author_ = 'wombat'
+
+
+class Solver:
+    def demo(self):
+        while True:
+            a = int(input("a "))
+            b = int(input("b "))
+            c = int(input("c ")) + b
+            print(a, b, c)
+
+
+Solver().demo()
